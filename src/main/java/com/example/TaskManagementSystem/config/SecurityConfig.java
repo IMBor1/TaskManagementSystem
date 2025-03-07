86bee04d06ac3eed0d13fb91ec1ee8f7eca8256d
IMBor1 <ilacerber1@gmail.com>
2025-03-07 16:28:20 +0300

    error

--- src/main/java/com/example/TaskManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/example/TaskManagementSystem/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                .requestMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
